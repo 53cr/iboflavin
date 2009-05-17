9629014839d69642151abd4743648a2d3a5e46be
Burke Libbey <burke@burkelibbey.org>
2009-05-17 17:42:41 -0500

    konami code

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,14 @@
+if ( window.addEventListener ) {
+  var state = 0, konami = [38,38,40,40,37,39,37,39,66,65];
+  window.addEventListener("keydown", function(e) {
+    if ( e.keyCode == konami[state] ) state++;
+    else state = 0;
+    if ( state == 10 ) window.location = "http://53cr.com/secret";
+  }, true);
+}
+
+
+
 $(document).ready(function() {
 
   $('#cloudpanel').animate({
