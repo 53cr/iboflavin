7b84e03ced5cf8b5dc9357a5229406ba2dd30bde
Burke Libbey <burke@burkelibbey.org>
2009-05-17 02:51:58 -0500

    lots of layout stuff

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function() {
+
+  $('#cloudpanel').animate({
+    "background-position": "707px"
+  },150000, "linear");
+
+});
