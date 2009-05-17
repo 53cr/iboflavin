dde023d3bbc12b0e0e54252edbffed84727e4e9d
Ryan Neufeld <neufelry@gmail.com>
2009-05-16 23:54:54 -0500

    Got tests kinda working for the time being, much improvement to be had

---
+++ test/unit/user_input_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+require 'user_input'
+
+class UserInputTest < ActiveSupport::TestCase
+  def test_cases
+    {
+      "Two bottles of Heineken" => "Two:::bottles of Heineken",
+      "Five eggs" => "Five:::eggs"
+    }.each do |key,value|
+      self.instance_eval <<-EOS
+         assert_equal UserInput.new("#{key}").mark_split, "#{value}"
+      EOS
+    end
+  end
+end
+
