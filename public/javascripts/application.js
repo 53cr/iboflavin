87a8201d2cbc2d26a56c10a8101b79221721289e
Burke Libbey <burke@burkelibbey.org>
2009-05-17 15:14:16 -0500

    added default text to question

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,24 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
-
 $(document).ready(function() {
 
   $('#cloudpanel').animate({
     "background-position": "707px"
-  },150000, "linear");
+  }, 150000, "linear");
+
+  var def = "Four Pan-Galactic Gargle Blasters";
+
+  $("#thequestion input.text").
+    attr('value', def).
+    focus(function() {
+      if (def == $(this).attr('value'))
+        $(this).
+          attr('value', '').
+          css('color', '#444');
+    }).
+    blur(function() {
+      if ("" == $(this).attr('value'))
+        $(this).
+          attr('value', def).
+          css('color', '#999');
+    });
 
 });
