fa2985a65fe40fc9d5fe41a0dddf8452ccee3990
Burke Libbey <burke@burkelibbey.org>
2009-05-18 04:34:25 -0500

    compass

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,8 +1,5 @@
 require 'compass'
 # If you have any compass plugins, require them here.
-Compass.configuration do |config|
-  config.project_path = RAILS_ROOT
-  config.sass_dir = "app/stylesheets"
-  config.css_dir = "public/stylesheets/compiled"
-end
+Compass.configuration.parse(File.join(RAILS_ROOT, "config", "compass.config"))
+Compass.configuration.environment = RAILS_ENV.to_sym
 Compass.configure_sass_plugin!
