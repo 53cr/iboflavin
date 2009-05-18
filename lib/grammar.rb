e401d609b7c6d1e84f3292d46a04b0df62a2c34c
Ryan Neufeld <neufelry@gmail.com>
2009-05-18 11:30:31 -0500

    Added comments to lib/grammar.rb

--- lib/grammar.rb
+++ lib/grammar.rb
@@ -19,31 +19,48 @@ module Grammar
     # If no Amount Part is given, we assume one serving, and hope the DB
     # provides that information.
 
+    # Collapse multiple spaces or dashses to singular ones
     input = input.gsub(/([\s-]+)/) do
       $1[0..0]
     end
 
+    # Have split parts determine the object and quantifier for the input
     quantifier, object = split_parts(input)
     
-    if quantifier
-      count, unit = parse_quantifier(quantifier)
-    else
-      count, unit = 1, Vocabulary::Serving
-    end
+    # Given a quantifier exists or not, assign the count and unit
+    count, unit = if quantifier
+                    parse_quantifier(quantifier)
+                  else
+                    1, Vocabulary::Serving # default to 1 serving
+                  end
       
     return [count, unit, object]
+    # RN: I think long term we want to return something like:
+    #  [
+    #   { food: object, count: count, unit: unit},
+    #   ...
+    #  ]
+    #
+    # OR
+    #   {
+    #     object => { count => unit }
+    #   }
     
   end
 
   # Given a quantifier, return [count, unit], eg. "3.5oz of" => [0.103507353, Litre]
   def self.parse_quantifier(quantifier)
+    # Split on spaces/dashes, then map false or unit+count to each word
     values = quantifier.split(/[\s-]+/).map{|e|Vocabulary::quantifier_value(e)}
     
+    # take product of the values to get a total
+    # TODO: Make hold up under strange quantifiers (eg "2 and 5 burgers")
     total = values.inject(1) do |acc, num|
       (acc *= num) if num.is_a? Numeric
       acc
     end
 
+    # Seperate out count and unit and return
     count = total.to_f
     unit  = total.class
     unit  = Vocabulary::Serving if [Fixnum, Float].include?(unit)
@@ -52,20 +69,24 @@ module Grammar
   
   # Split the quantifier from the object and return [quantifier, object]
   def self.split_parts(input)
+    # Get list of tokens from input by splitting on spaces/dashes
     tokens = input.downcase.split(/[\s-]+/)
 
+    # Build marks, the list of tokens that appear to be quantifiers
     marks = tokens.inject([]) do |acc, token|
       acc << Vocabulary::quantifier_value(token)
     end
 
-    # not ruby-esque, but more efficient. Deal with it.
-    str = ""
+    # Build the quantifier back up again, to be used for splitting input
+      # not ruby-esque, but more efficient. Deal with it.
+    proposed_quantifier = ""
     marks.size.times do |i|
       break unless marks[i]
-      str << tokens[i] << " "
+      proposed_quantifier << tokens[i] << " "
     end
     
-    splitting_point = str.size
+    # Split at the edge of the proposed quantifier, returning [quantifier, object]
+    splitting_point = proposed_quantifier.size
     
     term = splitting_point-2
     quantifier = (term<0) ? "" : input[0..term]
