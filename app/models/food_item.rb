308e91877bb736f6b86b36219e84096d1752b407
Burke Libbey <burke@burkelibbey.org>
2009-05-17 20:12:54 -0500

    production db

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -1,8 +1,16 @@
 class FoodItem < ActiveRecord::Base
-
+  
   belongs_to :food_group
 
   has_many :food_item_nutrients
   has_many :nutrients, :through => :food_item_nutrients
 
+
+  define_index do
+
+    indexes name
+
+  end
+
+  
 end
