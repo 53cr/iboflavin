d9f278c51d850525937c845bad43592d70ffbeed
Burke Libbey <burke@burkelibbey.org>
2009-05-22 00:49:59 -0500

    fixed routing problem

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :entry_matches
   map.resources :entries
 
+  Chromium53::Routes.add_to(map) 
+
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
-
-  Chromium53::Routes.add_to(map) 
 end
