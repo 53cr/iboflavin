f5dfa32ef9a2ba582e792fbd211d7be2f42184c9
Ryan Neufeld <neufelry@gmail.com>
2009-05-23 18:49:19 -0500

    Updated specs to be more readable in -f n mode

--- spec/lib/enumerable_spec.rb
+++ spec/lib/enumerable_spec.rb
@@ -12,63 +12,73 @@ describe Enumerable do
     [1,2,3].should respond_to :includes_all?
     {:name => "Jeff"}.should respond_to :includes_all?
   end
-  
-  context "#includes_all? extensions" do
-    it "should include? singular element" do
-      [1,2,3].should include_all_of 1
+  context "#includes_all? should return" do
+    
+    context "true when given correct" do
+      it "single argument" do
+        [1,2,3].should include_all_of 1
+      end
+    
+      it "multiple arguments" do
+        [1,2,3].should include_all_of 1, 2
+      end
+    
+      it "array with multiple elements" do
+        [1,2,3].should include_all_of [1, 2]
+      end
+    
+      it "key that exists in the hash" do
+        { :first => "ryan", :last => "neufeld"}.should
+          include_all_of :first
+      end
+    
+      it "multiple keys that exist in the hash" do
+        { :first => "ryan", :last => "neufeld"}.should
+          include_all_of :first, :last
+      end
+    
+      it "array of keys that exist in the hash" do
+        { :first => "ryan", :last => "neufeld"}.should 
+          include_all_of [:first, :last]
+      end
+    
+      it "multiple levels of nested items and arrays" do
+        [1,2,3].should include_all_of [[1,2],3]
+      end
     end
     
-    it "should include argument delimitted elements" do
-      [1,2,3].should include_all_of 1, 2
-    end
+    context "false when given incorrect" do
+      # Now for the _not_'s
+      it "single argument" do
+        [1,2,3].should_not include_all_of 4
+      end
     
-    it "should include elements in an array" do
-      [1,2,3].should include_all_of [1, 2]
-    end
+      it "multiple arguments" do
+        [1,2,3].should_not include_all_of 1, 4
+      end
     
-    it "should include single key of a hash" do
-      { :first => "ryan", :last => "neufeld"}.should include_all_of :first
-    end
+      it "array with multiple elements" do
+        [1,2,3].should_not include_all_of [1, 4]
+      end
     
-    it "should include argument delimitted keys of a hash" do
-      { :first => "ryan", :last => "neufeld"}.should include_all_of :first, :last
-    end
+      it "key that exists in the hash" do
+        { :last => "neufeld"}.should_not 
+          include_all_of :first
+      end
     
-    it "should include array of keys of a hash" do
-      { :first => "ryan", :last => "neufeld"}.should include_all_of [:first, :last]
-    end
+      it "multiple keys that exist in the hash" do
+        { :last => "neufeld"}.should_not 
+          include_all_of :first, :last
+      end
     
-    it "should traverse multiple levels of nesting" do
-      [1,2,3].should include_all_of [[1,2],3]
-    end
+      it "array of keys that exist in the hash" do
+        { :last => "neufeld"}.should_not 
+          include_all_of [:first, :last]
+      end
     
-    # Now for the _not_'s
-    it "false when including singular element" do
-      [1,2,3].should_not include_all_of 4
-    end
-    
-    it "false when including argument delimitted elements" do
-      [1,2,3].should_not include_all_of 1, 4
-    end
-    
-    it "false when including elements in an array" do
-      [1,2,3].should_not include_all_of [1, 4]
-    end
-    
-    it "false when including single key of a hash" do
-      { :first => "ryan", :last => "neufeld"}.should_not include_all_of :third
-    end
-    
-    it "false when including argument delimitted keys of a hash" do
-      { :first => "ryan", :last => "neufeld"}.should_not include_all_of :first, :third
-    end
-    
-    it "false when including array of keys of a hash" do
-      { :first => "ryan", :last => "neufeld"}.should_not include_all_of [:first, :third]
-    end
-    
-    it "false when traversing multiple levels of nesting" do
-      [1,2,3].should_not include_all_of [[1,2],4]
+      it "multiple levels of nested items and arrays" do
+        [1,2,3].should_not include_all_of [[1,2],4]
+      end
     end
   end
 end
