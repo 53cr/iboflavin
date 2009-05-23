05cb285745bf2b03ade5b665a560f42d08f6a0da
Ryan Neufeld <neufelry@gmail.com>
2009-05-22 23:25:17 -0500

    Added basic nutrition requirements spec

---
+++ spec/nutrition/nutrition_spec.rb
@@ -0,0 +1,12 @@
+require File.join(File.dirname(__FILE__), "/../spec_helper")
+require 'nutrition'
+
+module Nutrition
+  describe "Nutrition" do
+    context "the truth" do
+      it "should be true" do
+        true.should be true
+      end
+    end
+  end
+end
