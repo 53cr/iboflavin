84b5d838ee9f179c24baf9dafd4b6fe4a8be5469
Ryan Neufeld <neufelry@gmail.com>
2009-05-24 14:45:22 -0500

    Added models spec folder and user spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require File.join(File.dirname(__FILE__), "/../spec_helper")
+
+describe "A User", :type => :model do
+  before(:each) do
+    @user = User.new
+  end
+  it "cannot be older than 999" do
+    pending
+    @user.age = 1000
+    @user.should be_invalid
+  end
+end
