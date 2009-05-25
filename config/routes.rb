d23b55f7cdc4a947f9b2885c71c1bfbc5ecc10a8
Burke Libbey <burke@burkelibbey.org>
2009-05-24 20:22:59 -0500

    well, umm... auth *technically* works...

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :entry_matches
   map.resources :entries
 
+  map.resource :user_session
+
+  map.rpx_login 'login/rpx', :controller => 'user_sessions', :action => 'create'
+  
+  map.resource :account, :controller => "users"
+  map.resources :users
+  
   map.root :controller => 'static'
 
   map.connect ':controller/:action/:id'
