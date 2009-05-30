f1465169878ded831a95569601aaf7bb70a039b1
Burke Libbey <burke@burkelibbey.org>
2009-05-29 21:50:27 -0500

    layout changes

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -4,17 +4,22 @@ class Entry < ActiveRecord::Base
 
   belongs_to :user
   has_many :entry_matches
-  
+
   private
   def create_entry_matches
-
+          
     items = Grammar::parse(self.input)
 
     items.each do |item|
-      EntryMatch.create!({:user_id => self.user_id, :entry_id => self.id, :search => item[2], :amount => item[0], :unit => item[1].to_s})
+      EntryMatch.create!({
+              :user_id => self.user_id,
+              :entry_id => self.id,
+              :search => item[2],
+              :amount => item[0],
+              :unit => item[1].to_s
+      })
     end
     
   end
 
-  
 end
