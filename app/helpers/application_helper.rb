f6a379454d2a2c3c73264636e112adf701921ab3
Burke Libbey <burke@burkelibbey.org>
2009-06-07 17:19:35 -0500

    fixed gravatar default

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
 
   def gravatar(email)
     hash = Digest::MD5.hexdigest(email)
-    "http://www.gravatar.com/avatar/#{hash}?s=36&r=pg&d=http%3A%2F%2Fiboflav.in%2Fimages%2Fdefault_avatar.png"
+    "http://www.gravatar.com/avatar/#{hash}?s=36&r=pg&d=%2Fimages%2Fdefault_avatar.png"
   end
 
 end
