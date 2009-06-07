a072377fac5be7a695cbb4f5043666081b38b901
Burke Libbey <burke@burkelibbey.org>
2009-06-06 20:40:36 -0500

    front page redirects to new entry if user is logged in

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,10 +3,10 @@ class StaticController < ApplicationController
 #
 # caches_page :index                   
 
-  
-  
   def index
-    
+    if @current_user
+      redirect_to new_entry_url
+    end
   end
   
 end
