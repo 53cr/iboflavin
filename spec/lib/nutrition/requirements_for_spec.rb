5c005674c2eb8a444ffe911d19a842be616cb4e7
Ryan Neufeld <neufelry@gmail.com>
2009-06-07 20:30:31 -0500

    Specs for nutrient and nutrition::requirement nutrients to have same unit

--- spec/lib/nutrition/requirements_for_spec.rb
+++ spec/lib/nutrition/requirements_for_spec.rb
@@ -7,7 +7,14 @@ module Nutrition
       
     end
     
-    it "should have the same units as the Nutrient table"
+    # All Nutrients in Nutrition should have the same units as Nutrition
+    Nutrition::Requirements::NUTRIENT_IDS_TO_NUTRIENT.each do |id, symbol|
+      nutrient = Nutrient.find(id)
+      it "should have the same units for #{nutrient.description}" do
+        Nutrition::Requirements::UNITS[symbol].should == nutrient.unit.to_sym
+      end
+    end
+
     it "should return a value for a listed Nutrient"
     it "should return nil for an unlisted Nutrient"
   end
