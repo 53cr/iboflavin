e1362a42f2c067c328eb7df40da2b7aa88a2cca2
Burke Libbey <burke@burkelibbey.org>
2009-06-08 03:09:39 -0500

    added javascript_trigger helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,8 @@ module ApplicationHelper
     "http://www.gravatar.com/avatar/#{hash}?s=36&r=pg&d=http%3A%2F%2F#{APP[:host]}%2Fimages%2Fdefault_avatar.png"
   end
 
+  def javascript_trigger(trigger)
+    javascript_tag "$(document).trigger('#{trigger}')"
+  end
+  
 end
