83cc2dec6cb05a971a2a58db7691e284203b158c
Burke Libbey <burke@burkelibbey.org>
2009-08-02 23:17:01 -0500

    added new vocabulary stuff. not integrated yet.

---
+++ new_vocabulary/quantifier.rb
@@ -0,0 +1,177 @@
+# I should really read some sort of language parsing or compiler book. Oh well.
+
+# I'm using !!() here. Read it as to_boolean().
+
+module Vocabulary
+  module Quantifier
+
+    module Generic
+      
+      def self.parse(words)
+        rest = []
+        gq = []
+        
+        # this is a really ugly way to do it. 
+        # I couldn't think of anything slick. Sorry. :/
+        gq_over = false
+        words.each do |word|
+          if gq_over
+            rest << word
+          else
+            if parse_token(word)
+              gq << word
+            else
+              rest << word
+              gq_over = true
+            end
+          end
+        end
+        
+        gq.map!{|e|parse_token(e)}
+        
+        # now, we sort of have a poor-man's AST...
+        # "1 and 1/4 litre"
+        # => [1.0, :+, 0.25, [:litre, 1]]
+        
+        # get rid of fluff.
+        gq.reject!{|e|e==true || e==nil}
+        
+        # we COULD deal with stuff like "one and a half"... but we won't.
+        gq.reject!{|e|e==:+}
+        
+        total = [nil, 1]
+        gq.each do |term|
+          if term.kind_of?(Array)
+            total[0] = term[0]
+            total[1] *= term[1]
+          else
+            total[1] *= term
+          end
+        end
+
+        return [total, rest]
+      end
+
+      def self.parse_token(token)
+        number_part, word_part = nil
+        # allow integers, real numbers, and fractions. We'll do this naively.
+        token.sub(/^([\d\.\/]+)?\s*(.*)?$/) do 
+          number_part = (($1.nil? || $1.empty?) ? nil : $1)
+          word_part   = (($2.nil? || $2.empty?) ? nil : $2)
+        end
+
+        if number_part
+          # parse this number as a fraction of two floats.
+          frac_parts = number_part.split('/')
+          number_value  = frac_parts[0].to_f
+          (number_value = number_value / frac_parts[1].to_f) if frac_parts[1]
+        end
+        
+        if word_part
+          word_part = ActiveSupport::Inflector::singularize(word_part)
+        end
+
+        return number_value unless word_part
+
+        word_value = GQ_VOCAB[word_part] || Vocabulary::Units::UNITS[word_part]
+        # we have a word part, and we don't recognize it. Abort!
+        return nil unless word_value
+
+        # So at this point, we know we have a valid word_value, and we may or may not
+        # have an attached number_value.
+        if number_part
+          if word_value.is_a?(Array)
+            return [word_value[0], number_value*word_value[1]]
+          else
+            return word_value * number_value
+          end
+        else
+          return word_value
+        end
+      end
+      
+      MODIFIERS = {
+        "dozen"     => 12,
+        "several"   => 5,
+        "bunch"     => 7,
+        "few"       => 3,
+        "couple"    => 2,
+        "half"      => 0.5,
+        "third"     => 0.333333333,
+        "quarter"   => 0.25,
+        "fifth"     => 0.2,
+        "sixth"     => 0.166666667,
+        "seventh"   => 0.142857143,
+        "eigth"     => 0.125,
+        "ninth"     => 0.111111111,
+        "tenth"     => 0.1,
+        "twelfth"   => 0.083333333,
+        "fifteenth" => 0.066666667,
+        "twentieth" => 0.05,
+        "thirtieth" => 0.033333333,
+        "fiftieth"  => 0.02,
+        "hundredth" => 0.01
+      }
+      
+      FILLER = {
+        "a"   => true,
+        "an"  => true,
+        "and" => :+,
+        "the" => true
+      }
+
+      NUMBER_WORDS = {
+        "one"       => 1,
+        "two"       => 2,
+        "three"     => 3,
+        "four"      => 4,
+        "five"      => 5,
+        "six"       => 6,
+        "seven"     => 7,
+        "eight"     => 8,
+        "nine"      => 9,
+        "ten"       => 10,
+        "eleven"    => 11,
+        "twelve"    => 12,
+        "thirteen"  => 13,
+        "fourteen"  => 14,
+        "fifteen"   => 15,
+        "sixteen"   => 16,
+        "seventeen" => 17,
+        "eighteen"  => 18,
+        "nineteen"  => 19,
+        "twenty"    => 20,
+        "thirty"    => 30,
+        "forty"     => 40,
+        "fifty"     => 50,
+        "sixty"     => 60,
+        "seventy"   => 70,
+        "eighty"    => 80,
+        "ninety"    => 90,
+        "hundred"   => 100,
+        "thousand"  => 1000
+      }
+
+      GQ_VOCAB = {}.
+        merge(MODIFIERS).
+        merge(FILLER).
+        merge(NUMBER_WORDS)
+      
+    end
+
+    module Specific
+      KEYWORDS = { 
+        "large"   => true,
+        "extra"   => true,
+        "small"   => true,
+        "regular" => true,
+        "medium"  => true
+      }
+      
+      def self.is_keyword(word)
+        KEYWORDS[word]
+      end
+    end
+
+  end
+end
---
+++ new_vocabulary/vocabulary.rb
@@ -0,0 +1,93 @@
+
+# process: numerical quantifier should always be first. parse it, continue with rest.
+# look for words {small, large, extra large, extralarge, etc.}. remove, use for weight I guess?
+# look for the word "of". try searching s[0] as a weight for s[1]. If it doesn't exist, search for full phrase.
+
+require 'activesupport'
+
+require 'units'
+require 'metric'
+require 'numbers'
+require 'quantifier'
+
+module Vocabulary
+  
+  def self.parse(phrase)
+    # terminology:
+    #  - generic quantifier (GQ): a phrase describing an amount of food, 
+    #      applicable to any specific food, eg. "three", "2 litres", or "1.5lb"
+    #  - specific quantifier (SQ): a phrase describing an amount of food,
+    #      but for which we must determine the specific food before we know 
+    #      the quantity implied. eg. "stick". A stick of celery is very 
+    #      different from a stick of butter. Also, a "stick" of soup probably 
+    #      doesn't match, but maybe there's a soup called "stick of soup". You never know...
+    #  - food item (FI): A specific entry in the database.
+    #  - food search (FS): The string we'll use to search the DB for FIs.
+
+    # split on whitespace and hyphens.
+    words = phrase.split(/[\s-]+/)
+    
+    # First we look for a GQ. If it's given, it'll be at the start of the phrase.
+    # I can't think of a scenario where it would be given after an SQ / FS anyway.
+    generic_quantifier, words = Vocabulary::Quantifier::Generic::parse(words)
+    
+    # So now we've split the generic quantifier off the front of the phrase. We're 
+    # left with something like: gq:["3"], words:["large", "sticks", "of", "celery"]
+
+    # So now we have to find the specific quantifier (if any). We'll use a 
+    # two-part strategy. First, pull off any words from the front like "large",
+    # "small", etc. After that, split the input on the word "of", and use the first
+    # half (if any) as the rest of the SQ. eg. for "large stick of celery", 
+    # SQ=["large", "stick"], and FS=["celery"]
+
+    specific_quantifier = []
+    food_search = nil
+    
+    # however, if the generic quantifier already has units, we're not going to 
+    # go hunting for a SQ!
+
+    if ((!generic_quantifier.is_a?(Array)) || generic_quantifier[0].nil?)
+
+      # SQ Step 1: freebie words (large, small, etc.)
+      while Vocabulary::Quantifier::Specific::is_keyword(words[0])
+        specific_quantifier << words.shift
+      end
+      
+      # SQ Step 2: split on "of".
+      if (of_idx = words.index("of"))
+        # eg. "three cups of celery"
+        specific_quantifier.concat(words[0...of_idx]) #=> "three cups"
+        food_search = words[of_idx+1..-1]             #=> "celery"
+      end
+      
+    end
+    
+    # if we haven't already set it...
+    food_search ||= words
+    food_search.shift if food_search[0] == "of"
+    
+    # Now, this may not be the real solution. For example, "cream of mushroom soup"
+    # will try to use "cream" as a specific quantifier, when really, it should be 
+    # part of the food search. However, we'll let the calling context deal with that,
+    # since if the SQ turns up no hits, it's just a matter of joining SQ and FS and
+    # trying again. 
+
+    return [generic_quantifier, specific_quantifier, food_search]
+  end
+  
+end
+
+
+=begin
+2 large fries
+three sticks of celery
+half a stick of butter
+one and a half big macs
+2.5L rum
+3 litres gin
+35g mixed nuts
+10oz beef
+10oz coffee
+1/4 cup cranberries
+an apple
+=end
